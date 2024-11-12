16e501a025cbfe7e1b9d208e5e0cd70616deea40
Joselyn-Alejandra-Valenzuela-0552 <164534000+Joselyn-Alejandra-Valenzuela-0412@users.noreply.github.com>
2024-11-12 13:09:36 -0600

    Add files via upload
    subi archivos de mi tienda CRUD 12 DE NOVIEMBRE 2024

---
+++ act3_CRUD_PandaManga/editorial_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EditorialAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'editorial_app'
---
+++ act3_CRUD_PandaManga/editorial_app/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render,redirect
+from .models import Editorial
+# Create your views here.
+
+def inicio_vista(request):
+    miseditoriales=Editorial.objects.all
+    return render(request,"gestionarEditorial.html",{"laseditoriales":miseditoriales})
+
+
+def registrarEditorial(request):
+    id_edit=request.POST["txtIdeditorial"]
+    nombre=request.POST["txtnombre"]
+    pais=request.POST["txtpais"]
+    anio_fundac=request.POST["dateaniofundac"]
+    fundador=request.POST["txtfundador"]
+    correo=request.POST["txtcorreo"]
+    proyectos=request.POST["txtproyectos"]
+
+    guardarEditorial=Editorial.objects.create(id_edit=id_edit,nombre=nombre,pais=pais,anio_fundac=anio_fundac,fundador=fundador,correo=correo,proyectos=proyectos)
+    return redirect("/")
+
+def seleccionarEditorial(request,id_edit):
+    editorial=Editorial.objects.get(id_edit=id_edit)
+    return render(request,"editareditorial.html",{"laseditoriales":editorial})
+
+def editarEditorial(request):
+    id_edit=request.POST["txtIdeditorial"]
+    nombre=request.POST["txtnombre"]
+    pais=request.POST["txtpais"]
+    anio_fundac=request.POST["dateaniofundac"]
+    fundador=request.POST["txtfundador"]
+    correo=request.POST["txtcorreo"]
+    proyectos=request.POST["txtproyectos"]
+
+    editorial=Editorial.objects.get(id_edit=id_edit)
+    editorial.nombre=nombre
+    editorial.pais=pais
+    editorial.anio_fundac=anio_fundac
+    editorial.fundador=fundador
+    editorial.correo=correo
+    editorial.proyectos=proyectos
+    
+    editorial.save()
+    return redirect("/")
+
+def borrarEditorial(request,id_edit):
+    editorial=Editorial.objects.get(id_edit=id_edit)
+    editorial.delete()
+    return redirect("/")
